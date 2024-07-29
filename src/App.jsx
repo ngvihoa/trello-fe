2f7b3f45dbdb7de8a2ee1a11244545ec11bee557
NVH <93785793+ngvihoa@users.noreply.github.com>
2024-07-29 10:10:48 +0700

    refactor project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,11 @@
-import { useColorScheme } from '@mui/material/styles'
-import MenuItem from '@mui/material/MenuItem'
-import FormControl from '@mui/material/FormControl'
-import Select from '@mui/material/Select'
-import LightModeIcon from '@mui/icons-material/LightMode'
-import DarkModeIcon from '@mui/icons-material/DarkMode'
-import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
-import Box from '@mui/material/Box'
-import Container from '@mui/material/Container'
-
-function ModeSelect() {
-	const { mode, setMode } = useColorScheme()
-
-	const handleChange = (event) => {
-		setMode(event.target.value)
-	}
-
-	return (
-		<FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-			<Select id="select-mode" value={mode} onChange={handleChange}>
-				<MenuItem value="light">
-					<Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-						<LightModeIcon fontSize="small" /> Light
-					</Box>
-				</MenuItem>
-				<MenuItem value="dark">
-					<Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-						<DarkModeIcon fontSize="small" /> Dark
-					</Box>
-				</MenuItem>
-				<MenuItem value="system">
-					<Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-						<SettingsBrightnessIcon fontSize="small" /> System
-					</Box>
-				</MenuItem>
-			</Select>
-		</FormControl>
-	)
-}
+import Board from './pages/Boards/_id'
 
 function App() {
 	return (
-		<Container
-			disableGutters
-			maxWidth={false}
-			sx={{
-				height: '100dvh'
-			}}
-		>
-			<Box
-				sx={{
-					backgroundColor: 'primary.light',
-					width: '100%',
-					height: (theme) => theme.trello.appBarHeight,
-					display: 'flex',
-					alignItems: 'center'
-				}}
-			>
-				<ModeSelect />
-			</Box>
-			<Box
-				sx={{
-					backgroundColor: 'primary.dark',
-					width: '100%',
-					height: (theme) => theme.trello.boardBarHeight,
-					display: 'flex',
-					alignItems: 'center'
-				}}
-			>
-				{/* Board Navbar */}
-			</Box>
-			<Box
-				sx={{
-					backgroundColor: 'primary.main',
-					width: '100%',
-					height: (theme) =>
-						`calc(100dvh - ${theme.trello.boardBarHeight} - ${theme.trello.appBarHeight})`,
-					display: 'flex'
-				}}
-			>
-				Board content
-			</Box>
-		</Container>
+		<>
+			{/* Router config */}
+			<Board />
+		</>
 	)
 }
 
