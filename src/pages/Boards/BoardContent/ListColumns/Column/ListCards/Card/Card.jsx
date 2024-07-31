d521df2dde8e4936e1214be101eb203fd7630922
NVH <93785793+ngvihoa@users.noreply.github.com>
2024-07-31 10:13:32 +0700

    mock data and apply sort algorithm to columns and cards

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -8,28 +8,12 @@ import GroupIcon from '@mui/icons-material/Group'
 import ChatIcon from '@mui/icons-material/Chat'
 import AttachFileIcon from '@mui/icons-material/AttachFile'
 
-function Card({ temporaryHeightMedia }) {
-	if (temporaryHeightMedia) {
+function Card({ card }) {
+	const shouldShowCardAction = () => {
 		return (
-			<MuiCard
-				sx={{
-					borderRadius: 2,
-					cursor: 'pointer',
-					boxShadow: '0 1px 1px rgba(0, 0, 0, 0.2)',
-					overflow: 'unset'
-				}}
-			>
-				<CardContent
-					sx={{
-						p: 1.5,
-						'&:last-child': {
-							p: 1.5
-						}
-					}}
-				>
-					<Typography>Card media</Typography>
-				</CardContent>
-			</MuiCard>
+			!!card?.memberIds?.length ||
+			!!card?.comments?.length ||
+			!!card?.attachments?.length
 		)
 	}
 	return (
@@ -41,12 +25,9 @@ function Card({ temporaryHeightMedia }) {
 				overflow: 'unset'
 			}}
 		>
-			<CardMedia
-				component="img"
-				alt="green iguana"
-				height="140"
-				image="https://media.istockphoto.com/id/1227023176/vi/anh/ng%C6%B0%E1%BB%9Di-ph%E1%BB%A5-n%E1%BB%AF-tr%E1%BA%BB-ch%C3%A8o-thuy%E1%BB%81n-kayak-qua-h%E1%BB%93-n%C3%BAi-l%C3%BAc-b%C3%ACnh-minh.jpg?s=1024x1024&w=is&k=20&c=cPMpJWYlb0byDNdbHxzzKpQd01U--VTQrfpoeAiWkYc="
-			/>
+			{card?.cover && (
+				<CardMedia component="img" height="140" image={card.cover} />
+			)}
 			<CardContent
 				sx={{
 					p: 1.5,
@@ -55,19 +36,27 @@ function Card({ temporaryHeightMedia }) {
 					}
 				}}
 			>
-				<Typography>Card media</Typography>
+				<Typography>{card.title}</Typography>
 			</CardContent>
-			<CardActions sx={{ p: '0 4px 8px 4px' }}>
-				<Button size="small" startIcon={<GroupIcon />}>
-					20
-				</Button>
-				<Button size="small" startIcon={<ChatIcon />}>
-					5
-				</Button>
-				<Button size="small" startIcon={<AttachFileIcon />}>
-					15
-				</Button>
-			</CardActions>
+			{shouldShowCardAction() && (
+				<CardActions sx={{ p: '0 4px 8px 4px' }}>
+					{card?.memberIds?.length && (
+						<Button size="small" startIcon={<GroupIcon />}>
+							{card.memberIds.length}
+						</Button>
+					)}
+					{card?.comments?.length && (
+						<Button size="small" startIcon={<ChatIcon />}>
+							{card.comments.length}
+						</Button>
+					)}
+					{card?.attachments?.length && (
+						<Button size="small" startIcon={<AttachFileIcon />}>
+							{card.attachments.length}
+						</Button>
+					)}
+				</CardActions>
+			)}
 		</MuiCard>
 	)
 }
